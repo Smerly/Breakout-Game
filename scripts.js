da8f56dcbe6417dbafd552fc7659a7b25e1dd57e
Ernest Cheung <ErnestCheung02@gmail.com>
2020-10-27 17:59:29 -0700

    FInished 3rd step and added ESLint

--- scripts.js
+++ scripts.js
@@ -1,37 +1,15 @@
-// var canvas = document.getElementById("myCanvas");
-// var ctx = canvas.getContext("2d");
-
-// function draw() {
-//   // drawing code
-
-//   var x = canvas.width / 2;
-//   var y = canvas.height - 30;
-
-//   var dx = 2;
-//   var dy = -2;
-
-//   ctx.beginPath();
-//   ctx.arc(x, y, 10, 0, Math.PI * 2);
-//   ctx.fillStyle = "#0095DD";
-//   ctx.fill();
-//   ctx.closePath();
-//   x += dx;
-//   y += dy;
-// }
-
-// setInterval(draw, 10);
-
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-var dx = 1.5;
-var dy = -1.5;
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+let ballRadius = 10;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 1.5;
+let dy = -1.5;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095DD';
   ctx.fill();
   ctx.closePath();
 }
@@ -41,6 +19,14 @@ function draw() {
   drawBall();
   x += dx;
   y += dy;
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
 }
 
 setInterval(draw, 10);
